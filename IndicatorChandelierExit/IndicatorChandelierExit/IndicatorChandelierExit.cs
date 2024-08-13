8a6c1844371e46a7a7347cf2d3ed87268e4782b4
Viktoriia <tsuki171097@gmail.com>
2024-08-13 13:10:09 +0300

    Add files via upload

---
+++ IndicatorChandelierExit/IndicatorChandelierExit/IndicatorChandelierExit.cs
@@ -0,0 +1,68 @@
+// Copyright QUANTOWER LLC. © 2017-2023. All rights reserved.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using TradingPlatform.BusinessLayer;
+using static System.Net.Mime.MediaTypeNames;
+using TradingPlatform.BusinessLayer.Integration;
+using TradingPlatform.BusinessLayer.Utils;
+using TradingPlatform.BusinessLayer.Chart;
+
+namespace IndicatorChandelierExit
+{
+	public class IndicatorChandelierExit : Indicator
+    {
+        [InputParameter("ATR Period", 0, 1, 9999)]
+        public int Period = 22;
+        [InputParameter("ATR Multiplier", 0, 1, 9999)]
+        public double multipl = 3;
+
+        public override string ShortName => $"CE ({this.Period}; {this.multipl})";
+
+        private double longStop, longStopPrev, shortStop, shortStopPrev;
+        private int dir;
+
+        private Indicator atr;
+        public IndicatorChandelierExit()
+            : base()
+        {
+            Name = "Chandelier Exit";
+            Description = "The Chandelier Exit is a popular tool among traders used to help determine appropriate stop loss levels.";
+
+            AddLineSeries("Sell", Color.Red, 1, LineStyle.Solid);
+            AddLineSeries("Buy", Color.Green, 1, LineStyle.Solid);
+
+            SeparateWindow = false;
+        }
+
+        protected override void OnInit()
+        {
+            this.atr = Core.Indicators.BuiltIn.ATR(this.Period, MaMode.SMA, Indicator.DEFAULT_CALCULATION_TYPE);
+            this.AddIndicator(this.atr);
+            dir = 1;
+            longStop = High() - atr.GetValue() * multipl;
+            shortStop = Low() + atr.GetValue() * multipl;
+        }
+
+        protected override void OnUpdate(UpdateArgs args)
+        {
+            longStopPrev = longStop;
+            longStop = High() - atr.GetValue() * multipl;
+            longStop = (GetPrice(PriceType.Close) > longStopPrev) ? Math.Max(longStop, longStopPrev) : longStop;
+
+            shortStopPrev = shortStop;
+            shortStop = Low() + atr.GetValue() * multipl;
+            shortStop = (GetPrice(PriceType.Close) < shortStopPrev) ? Math.Min(shortStop, shortStopPrev) : shortStop;
+
+            if (GetPrice(PriceType.Close) > shortStopPrev)
+                dir = 1;
+            else if (GetPrice(PriceType.Close) < longStopPrev)
+                dir = -1;
+
+            if (dir == 1) SetValue(longStopPrev, 1);
+            else SetValue(shortStopPrev, 0);
+        }
+    }
+}
